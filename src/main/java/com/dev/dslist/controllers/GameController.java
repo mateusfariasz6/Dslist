c80c59d0ce874f66823e631e0a6eaf1a596c7bd1
Mateus Farias Costa <mateusfariasz86@gmail.com>
2023-05-09 17:10:31 -0300

    feat(Game): Adding a method to list all games via a get request.

--- src/main/java/com/dev/dslist/controllers/GameController.java
+++ src/main/java/com/dev/dslist/controllers/GameController.java
@@ -1,10 +1,23 @@
 package com.dev.dslist.controllers;
 
+import com.dev.dslist.dtos.GameMinGetRequestDto;
+import com.dev.dslist.services.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/game")
+@RequiredArgsConstructor
 public class GameController {
-
+    private final GameService gameService;
+    @GetMapping
+    public ResponseEntity<List<GameMinGetRequestDto>> listAll(Pageable pageable){
+        return ResponseEntity.ok(gameService.listAll(pageable));
+    }
 }
--- src/main/java/com/dev/dslist/services/GameService.java
+++ src/main/java/com/dev/dslist/services/GameService.java
@@ -1,8 +1,18 @@
 package com.dev.dslist.services;
 
+import com.dev.dslist.dtos.GameMinGetRequestDto;
+import com.dev.dslist.repositories.GameRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-@Service
-public class GameService {
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class GameService {
+    private final GameRepository gameRepository;
+    public List<GameMinGetRequestDto> listAll(Pageable pageable) {
+        return gameRepository.findAll(pageable).map(GameMinGetRequestDto::new).toList();
+    }
 }
