b1d4c78424184cf049e142fe84e0fe7054610d5e
Mateus Farias Costa <mateusfariasz86@gmail.com>
2023-05-09 16:05:28 -0300

    feat(Game): Adding the Game class in the entity package.

---
+++ src/main/java/com/dev/dslist/entities/Game.java
@@ -0,0 +1,29 @@
+package com.dev.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_games")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    @Column(columnDefinition = "TEXT")
+    private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+
+}
